7c57ea25d3506f9dd671400147d4a4d50db52419
Muyiwa Olalekan <muyiwa.olalekan@thebulb.africa>
2021-05-17 14:45:29 +0100

    Add Borrowed Item Controller to the application

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ItemController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> itemList = _dbContext.Items.ToList();
+            return Ok(itemList);
+        }
+    }
+}
